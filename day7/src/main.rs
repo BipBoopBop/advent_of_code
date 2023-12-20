15edf9c39dbb0cfd97ac069eb6104a5e451b6b50
Quentin <quentberthaud@gmail.com>
2023-12-20 17:04:19 +0100

    day7 part1

---
+++ day7/src/main.rs
@@ -0,0 +1,83 @@
+use std::fs;
+use fancy_regex::Regex;
+
+fn main() {
+    let input = fs::read_to_string("./data/input.txt").unwrap();
+    let part_one_result = part_one(input.clone());
+    let part_two_result = part_two(input.clone());
+
+    println!("part_one_result : {part_one_result}");
+    println!("part_two_result : {part_two_result}");
+}
+
+fn part_one(input_text: String) -> i32 {
+    let mut sum = 0;
+    let input: Vec<(&str, i32)> = input_text
+        .lines()
+        .map(|line| {
+            let l = line.split(" ").collect::<Vec<&str>>() ;
+            (l[0],
+            l[1].parse().unwrap())
+        }).collect();
+
+    let reg_type = [
+        Regex::new(r"(\w)\1{4}").unwrap(),
+        Regex::new(r"(\w)\1{3}").unwrap(),
+        Regex::new(r"(\w)\1\1(\w)\2|(\w)\3(\w)\4\4").unwrap(),
+        Regex::new(r"(\w)\1\1").unwrap(),
+        Regex::new(r"(\w)\1.*(\w)\2").unwrap(),
+        Regex::new(r"(\w)\1").unwrap()
+    ];
+
+    let mut hands_type: [Vec<usize>;7] = Default::default() ;
+
+    for (i,(hand, _)) in input.iter().enumerate(){
+        let mut hand_sort: Vec<char> = hand.chars().collect();
+        hand_sort.sort();
+        for (reg_i,regex) in reg_type.iter().enumerate(){
+            if regex.is_match(String::from_iter(hand_sort.clone()).as_str()).unwrap(){
+                hands_type[reg_type.len() - reg_i].push(i);
+                break;
+            }
+            else if reg_i == reg_type.len()-1{
+                hands_type[0].push(i);
+            }
+        }
+    }
+
+    for hand in &mut hands_type{
+        if hand.len() <= 1 {continue;}
+
+        hand.sort_by(|a,b| {
+            let hand_a = input[*a].0.chars();
+            let mut hand_b = input[*b].0.chars();
+            for a_c in hand_a{
+                let Some(b_c) = hand_b.nth(0) else {panic!("error sorting")}; 
+                if a_c == b_c {continue;} 
+                else if a_c == 'A' {return std::cmp::Ordering::Greater;}
+                else if b_c == 'A' {return std::cmp::Ordering::Less;}
+                else if a_c == 'K' {return std::cmp::Ordering::Greater;}
+                else if b_c == 'K' {return std::cmp::Ordering::Less;}
+                else if a_c == 'Q' {return std::cmp::Ordering::Greater;}
+                else if b_c == 'Q' {return std::cmp::Ordering::Less;}
+                else if a_c == 'J' {return std::cmp::Ordering::Greater;}
+                else if b_c == 'J' {return std::cmp::Ordering::Less;}
+                else if a_c == 'T' {return std::cmp::Ordering::Greater;}
+                else if b_c == 'T' {return std::cmp::Ordering::Less;}
+                else {return a_c.to_digit(10).cmp(&b_c.to_digit(10));}
+            }
+            std::cmp::Ordering::Equal
+        });
+
+    }
+
+    for (i,hand_i) in hands_type.into_iter().flatten().enumerate(){
+        let (_, bid) = input[hand_i];
+        sum += bid * (i as i32 + 1);
+    }
+    sum
+}
+
+fn part_two(input_text: String) -> i32 {
+    0
+}
