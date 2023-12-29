d63d64213ef46c3eecc69532b91b6e07abd6d653
Quentin <quentberthaud@gmail.com>
2023-12-29 21:52:42 +0100

    day15 part2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::{HashMap, BTreeMap}};
 
 fn main() {
     let input = fs::read_to_string("./data/input.txt").unwrap();
@@ -16,7 +16,6 @@ fn part_one(clone: String) -> u32 {
         let mut hash = 0;
         for c in steps.chars(){
             hash += c as u32;
-            println!("{c} : {hash}");
             hash *= 17;
             hash = hash %256;
         }
@@ -27,5 +26,40 @@ fn part_one(clone: String) -> u32 {
 }
 
 fn part_two(clone: String) -> i32 {
-    0
+    let mut sum = 0;
+    let mut light = vec![Vec::<(&str,usize)>::new(); 256];
+    for (i,steps) in clone.split(",").enumerate(){
+        let mut hash = 0;
+        for c in steps.chars().take_while(|c| c != &'=' && c != &'-'){
+            hash += c as u32;
+            hash *= 17;
+            hash = hash %256;
+        }
+        if steps.contains('=') {
+            let label_focal: Vec<&str> = steps.split('=').collect();
+            let label = label_focal[0];
+            let focal = label_focal[1].parse::<usize>().unwrap();
+            if light[hash as usize].iter().any(|(l,_)| l == &label){
+                let p = light[hash as usize].iter().position(|(l,_)| l == &label).unwrap();
+                if let Some(l_f) = light[hash as usize].get_mut(p) {
+                        *l_f = (label, focal);
+                }
+            }
+            else {
+                light[hash as usize].push((label,focal));
+            }
+        }
+        else{
+            let label_focal: Vec<&str> = steps.split('-').collect();
+            light[hash as usize].retain(|(label,_)| label != &label_focal[0]);
+        }
+    }
+
+    for (i,l) in light.iter().enumerate() {
+        for (ii,(_, focal)) in l.iter().enumerate() {
+            sum += (1+i) * (ii+1) * focal;
+        }
+    }
+
+    sum as i32
 }
